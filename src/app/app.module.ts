ab6687a10afbef040e7cb3f9195d9bab329b9977
varunmulinti <varun9.run@gmail.com>
2024-04-19 17:13:03 +0530

    added top navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent {
-
+  
+  
 }
